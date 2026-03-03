d9b5e1fcdf1d9c9a99eaa4215066431c9b7c47e3
Molitor István <istvan.molitor@centralmediacsoport.hu>
2026-03-03 15:41:50 +0100

    init

---
+++ src/Providers/SettingServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Molitor\Contact\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ContactServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}

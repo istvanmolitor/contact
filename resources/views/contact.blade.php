f3df17bb4d105cf1d0a6d07f6cf486984cc016ba
Molitor István <istvanmolitor@gmail.com>
2026-06-19 10:59:49 +0200

    refactor

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -7,22 +7,9 @@
     <x-ui::success-message />
 
     <x-ui::form action="{{ route('contact.submit') }}">
-
-        <div class="mb-4">
-            <x-ui::label for="name" value="Név" />
-            <x-ui::input type="text" name="name" id="name" :value="old('name')" required />
-            <x-ui::field-error name="name" />
-        </div>
-
-        <x-ui::email-field id="email" label="E-mail" :value="old('email')" required />
-        <x-ui::field-error name="email" />
-
-        <div class="mb-6">
-            <x-ui::label for="message" value="Üzenet" />
-            <x-ui::textarea name="message" id="message" required>{{ old('message') }}</x-ui::textarea>
-            <x-ui::field-error name="message" />
-        </div>
-
+        <x-ui::input-field name="name" label="Név" />
+        <x-ui::email-field name="email" label="E-mail" :value="auth()->user()?->email" />
+        <x-ui::textarea-field name="message" label="Üzenet" />
         <x-ui::submit-button>Küldés</x-ui::submit-button>
     </x-ui::form>
 </div>
